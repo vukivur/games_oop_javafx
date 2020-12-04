be65de714e286586c48f82133525f178f392fd3b
Kirill Vasin <vukivur@gmail.com>
2020-12-04 10:47:20 +0300

    2. Сокобан. Тестовое задание. [#412333]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,36 +2,41 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        return checkRow(board) || checkCell(board);
+        boolean result = false;
+        int diagonal = diagonal(board);
+        if (diagonal != -1) {
+            result = checkRow(board, diagonal) || checkCell(board, diagonal);
+        }
+        return result;
     }
 
-    public static boolean checkRow(int[][] board) {
-        boolean result = false;
-        for (int row = 0; row < board.length; row++) {
-            int count = 0;
-            for (int cell = 0; cell < board.length; cell++) {
-                if (board[row][cell] == 1) {
-                    count++;
-                }
-            }
-            if (count == 5) {
-                result = true;
+    public static int diagonal(int[][] board) {
+        int result = -1;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1) {
+                result = index;
             }
         }
         return result;
     }
 
-    public static boolean checkCell(int[][] board) {
-        boolean result = false;
-        for (int cell = 0; cell < board.length; cell++) {
-            int count = 0;
-            for (int row = 0; row < board.length; row++) {
-                if (board[row][cell] == 1) {
-                    count++;
-                }
+    public static boolean checkRow(int[][] board, int diagonal) {
+        boolean result = true;
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][diagonal] != 1) {
+                result = false;
+                break;
             }
-            if (count == 5) {
-                result = true;
+        }
+        return result;
+    }
+
+    public static boolean checkCell(int[][] board, int diagonal) {
+        boolean result = true;
+        for (int cell = 0; cell < board.length; cell++) {
+            if (board[diagonal][cell] != 1) {
+                result = false;
+                break;
             }
         }
         return result;
